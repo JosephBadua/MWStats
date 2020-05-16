872754c4a5cfafa7d0cefceecd7bf21801288d02
JosephBadua <josephbadua1006@gmail.com>
2020-05-15 17:22:47 -0700

    added all data iles

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,21 +1,6 @@
 module.exports = function(app) {
     // Load index page
     app.get("/", function(req, res) {
-        res.render("may");
-    });
-    app.get("/april", function(req, res) {
-        res.render("april");
-    });
-    app.get("/march", function(req, res) {
-        res.render("march");
-    });
-    app.get("/february", function(req, res) {
-        res.render("february");
-    });
-    app.get("/january", function(req, res) {
-        res.render("january");
-    });
-    app.get("/december", function(req, res) {
         res.render("index");
     });
 };
